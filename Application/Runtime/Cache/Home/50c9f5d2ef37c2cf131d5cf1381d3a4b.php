117cfe7df121749ef96099ad91e9756c44953f16
whg1221 <whg1221@126.com>
2016-01-23 11:59:56 +0800

    第一次提交

---
+++ Application/Runtime/Cache/Home/50c9f5d2ef37c2cf131d5cf1381d3a4b.php
@@ -0,0 +1,124 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>用户登录</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.4 -->
+    <link href="/Public/adminlte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="/Public/adminlte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link href="/Public/adminlte/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+    <!-- Customized CSS Style -->
+    <link href="/Public/css/customized.css" rel="stylesheet" type="text/css" />
+</head>
+<style type="text/css">
+body {
+    font-family: "Microsoft Yahei", verdana, helvetica, arial, sans-serif;
+}
+
+.verify {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    cursor: pointer;
+}
+</style>
+
+<body class="login-page">
+    <!-- header -->
+    <div class="col-md-4 line-horizontal"></div>
+    <div class="col-md-4 login-header">电池监控系统</div>
+    <div class="col-md-4 line-horizontal"></div>
+    <!-- header end -->
+    <!-- form -->
+    <div class="col-md-6">
+        <div class="login-left">
+            <div class="center">
+                <img src="/Public/adminlte/dist/img/logo.png" alt="上海航天电源技术有限公司" class="logo-img">
+            </div>
+            <p>上海航天电源技术有限公司</p>
+            <p>SHANGHAI AEROSPACE POWER</p>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="login-box-body login-box-body-patch">
+            <p class="login-box-msg login-box-msg-patch">管理员登录</p>
+            <form action="/index.php/Home/Login/login.html" method="post">
+                <div class="form-group has-feedback">
+                    <span>用户名：</span>
+                    <input type="text" name="username" class="form-control" />
+                    <!-- <span class="glyphicon glyphicon-user form-control-feedback"></span> -->
+                </div>
+                <div class="form-group has-feedback">
+                    <span>密码：</span>
+                    <input type="password" name="password" class="form-control" />
+                    <!-- <span class="glyphicon glyphicon-lock form-control-feedback"></span> -->
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck" style="color:#fff;">
+                            <label>
+                                <input type="checkbox" name="remember" value="1"> 记住我
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-customized btn-block btn-flat">登录</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <a href="#">忘记密码</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="register.html" class="text-center">注册新用户</a>
+        </div>
+    <!-- 测试弹出框 -->
+    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">测试弹出框</button>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">未连接WIFI模块，查看历史记录？</h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">退出</button>
+                    <button type="button" class="btn btn-primary">是</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 测试弹出框 结束 -->
+    </div>
+    <!-- form end -->
+    <!-- jQuery 2.1.4 -->
+    <script src="/Public/adminlte/plugins/jQuery/jQuery-2.1.4.min.js" type="text/javascript"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="/Public/adminlte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- iCheck -->
+    <script src="/Public/adminlte/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    <script>
+    $(function() {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    });
+    </script>
+</body>
+
+</html>
---
+++ Application/Runtime/Cache/Home/524a08806bc29449645d93a7a8f8e680.php
@@ -0,0 +1,110 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>新用户注册</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.4 -->
+    <link href="/Public/adminlte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome Icons -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="/Public/adminlte/dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <!-- iCheck -->
+    <link href="/Public/adminlte/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+    <!-- Customized CSS Style -->
+    <link href="/Public/css/customized.css" rel="stylesheet" type="text/css" />
+</head>
+<style type="text/css">
+body {
+    font-family: "Microsoft Yahei", verdana, helvetica, arial, sans-serif;
+}
+
+.verify {
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    cursor: pointer;
+}
+</style>
+
+<body class="login-page">
+    <!-- header -->
+    <div class="col-md-5 line-horizontal"></div>
+    <div class="col-md-2 login-header">电池监控系统</div>
+    <div class="col-md-5 line-horizontal"></div>
+    <!-- header end -->
+    <!-- form -->
+    <div class="col-md-6">
+        <div class="login-left">
+            <div class="center">
+                <img src="/Public/adminlte/dist/img/logo.png" alt="上海航天电源技术有限公司" class="logo-img">
+            </div>
+            <p>上海航天电源技术有限公司</p>
+            <p>SHANGHAI AEROSPACE POWER</p>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="login-box-body login-box-body-patch">
+            <p class="login-box-msg login-box-msg-patch">注册一个新用户</p>
+            <form action="/index.php/Home/Login/register.html" method="post">
+                <div class="form-group has-feedback">
+                    <span>用户名：</span>
+                    <input type="text" name="username" class="form-control" />
+                    <!-- <span class="glyphicon glyphicon-user form-control-feedback"></span> -->
+                </div>
+                <div class="form-group has-feedback">
+                    <span>密码：</span>
+                    <input type="password" name="password" class="form-control" />
+                    <!-- <span class="glyphicon glyphicon-lock form-control-feedback"></span> -->
+                </div>
+                <div class="form-group has-feedback">
+                    <span>确认密码：</span>
+                    <input type="password" name="repassword" class="form-control" />
+                    <!-- <span class="glyphicon glyphicon-lock form-control-feedback"></span> -->
+                </div>
+                <div class="row">
+                    <div class="col-xs-8" style="color:#fff;">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="agree" value="1"> 我同意 <a href="#">网站安全协议</a>
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-customized btn-block btn-flat">注册</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <a href="login.html" class="text-center">我已经注册了账户</a>
+        </div>
+    </div>
+    <!-- form end -->
+    <!-- jQuery 2.1.4 -->
+    <script src="/Public/adminlte/plugins/jQuery/jQuery-2.1.4.min.js" type="text/javascript"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="/Public/adminlte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- iCheck -->
+    <script src="/Public/adminlte/plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    <script>
+    $(function() {
+        $('input').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square-blue',
+            increaseArea: '20%' // optional
+        });
+    });
+    </script>
+</body>
+
+</html>
